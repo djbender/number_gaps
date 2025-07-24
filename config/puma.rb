f2786febfc611b42c7900c0720c93f0e2ab980f5
Derek Bender <170351+djbender@users.noreply.github.com>
2025-07-24 13:34:42 -0700

    Add debug

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,14 @@
+puts "=== PUMA CONFIG DEBUG ==="
+puts "RAILS_MAX_THREADS: #{ENV['RAILS_MAX_THREADS'].inspect}"
+puts "PORT: #{ENV['PORT'].inspect}"
+puts "WEB_CONCURRENCY: #{ENV['WEB_CONCURRENCY'].inspect}"
+
+threads_count = ENV.fetch("RAILS_MAX_THREADS", 3).to_i
+puts "Calculated threads_count: #{threads_count.inspect}"
+
+port_number = ENV.fetch("PORT", 3000).to_i
+puts "Calculated port: #{port_number.inspect}"
+puts "========================="
 workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
